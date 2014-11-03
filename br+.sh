802fa782633c4c5aca098583a4d3af36b791360e
Bart Polot <bart.polot+voyager@gmail.com>
2014-11-03 16:46:22 +0100

    Use direct hardware control for smoother setps, acceleration for multiple calls

--- br+.sh
+++ br+.sh
@@ -1,45 +1,115 @@
-#!/bin/sh
-#/sys/class/backlight/intel_backlight/brightness
+#!/bin/bash
 
-for i in `pgrep 'br[+|-].sh'`; do
-    echo $i
-    if [ $i != $$ ]; then
-	kill $i
+SYSFILE=/sys/class/backlight/intel_backlight/brightness
+GOAL_FILE="$XDG_RUNTIME_DIR/brightness/goal"
+
+
+# Get new brightness
+function get_br ()
+{
+    L=$1
+    if [ $L = 100 ]; then
+	NEW_BR=$MAX_BR
+    elif [ $L = 0 ]; then
+	NEW_BR=0
+    else
+	NEW_BR=`echo "scale=10; $MAX_BR*($L/100)^2+1" | bc`
     fi
-done
+    NEW_BR=${NEW_BR%%.*}
+}
 
-BR=`qdbus org.freedesktop.PowerManagement /org/kde/Solid/PowerManagement/Actions/BrightnessControl brightness`
+# Get current level
+function get_lvl ()
+{
+    if [ $CURR_BR = $MAX_BR ]; then
+	LEVEL=100
+    elif [ $CURR_BR = 0 ]; then
+	LEVEL=0
+    else
+	LEVEL=`echo "scale=10; 100*sqrt($CURR_BR/$MAX_BR)" | bc`
+    fi
+    LEVEL=${LEVEL%%.*}
+}
 
-DELTA=`echo "scale=0; sqrt($BR)" | bc -l`
-if [ $DELTA -lt 2 ]; then
-    DELTA=2
+
+################################################################
+
+mkdir -p `dirname $GOAL_FILE` || exit 1
+
+# Set goal
+if [ -f $GOAL_FILE ]; then
+    CURR_GOAL=`cat $GOAL_FILE`
+else
+    CURR_GOAL=$((100*CURR_BR/MAX_BR))
 fi
 
-echo $0 | grep '+'
+
+# Figure out step
+echo $0 | grep '+' > /dev/null
 if [ $? = 0 ]; then
-    NEW=$((BR+DELTA))
+    [ $CURR_GOAL = 100 ] && exit 0;
     STEP=1
 else
-    NEW=$((BR-DELTA))
+    [ $CURR_GOAL = 0 ] && exit 0;
     STEP=-1
 fi
 
 
-if [ $NEW -gt 100 ]; then
-    NEW=100
-fi
-if [ $NEW -lt 0 ]; then
-    NEW=0
-fi
-
-
-for i in `seq $BR $STEP $NEW`; do
-    qdbus \
-	org.freedesktop.PowerManagement \
-	/org/kde/Solid/PowerManagement/Actions/BrightnessControl \
-	setBrightness $i;
-    sleep 0.05
+# Kill other instances just in case
+for i in `pgrep 'br[+|-].sh'`; do
+    if [ $i != $$ ]; then
+	echo Other: $i
+	kill $i
+    fi
 done
 
+################################################################
 
+MAX_BR=`cat /sys/class/backlight/intel_backlight/max_brightness`
+CURR_BR=`cat /sys/class/backlight/intel_backlight/brightness`
+
+echo "" >> /tmp/br
+date >> /tmp/br
+echo "We are now at $CURR_BR of $MAX_BR" >> /tmp/br
+
+# Set goal
+NEW=$((CURR_GOAL+STEP*5))
+get_lvl
+LAG=$((CURR_GOAL-LEVEL))
+if [ $LAG != 0 ]; then
+    NEW=$((NEW+STEP*10))
+    echo "LAG! Step it up!"
+fi
+if [ $NEW -gt 100 ]; then
+    NEW=100
+elif [ $NEW -lt 0 ]; then
+    NEW=0
+fi
+echo $NEW > $GOAL_FILE
+
+
+[ $NEW -ge 100 ] && NEW_BR=$MAX_BR || get_br $NEW
+
+
+DELTA=$((NEW_BR-CURR_BR))
+[ $DELTA = 0 ] && exit 0
+STEP_BR=$((DELTA/8))
+[ $STEP_BR = 0 ] && STEP_BR=$STEP
+
+
+echo "We are at $LEVEL, goal is $NEW (was $CURR_GOAL), LAG: $LAG" >> /tmp/br
+echo "Delta is $DELTA" >> /tmp/br
+echo "Step is $STEP_BR" >> /tmp/br
+echo "$((CURR_BR+STEP_BR)) $STEP_BR $NEW_BR" >> /tmp/br
+#DELTA=`echo "scale=0; sqrt($BR)" | bc -l`
+
+for i in `seq $((CURR_BR+STEP_BR)) $STEP_BR $NEW_BR`; do
+    echo $i | sudo tee $SYSFILE
+done
+echo $NEW_BR | sudo tee $SYSFILE
+
+echo "" >> /tmp/br
+
+
+exit 0
 sleep 60
