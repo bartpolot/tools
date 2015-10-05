5d2b768ee7c76f469d1886151dea1578316460fb
Bart Polot <bart.polot+voyager@gmail.com>
2015-10-05 03:43:41 +0200

    Add a wrapper to start chromium with a ssh proxy

---
+++ remote_chromium.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+function start_remote {
+    f=$1
+    H=${f##*-}
+    CFGDIR=`ls -1d *-$H`
+    PORT=$((RANDOM % 15000 + 5000))
+    echo $H
+    
+    ssh -ND $PORT $H &
+    SSHPID=$!
+    
+    kdialog --title "Chromium at..." --passivepopup "$H" 2
+    sleep 1
+    chromium --high-dpi-support=1 --force-device-scale-factor=1 --proxy-server="socks5://localhost:$PORT" --user-data-dir=$CFGDIR
+    
+    kill $SSHPID
+}
+
+
+cd ~/.config/.remotes/
+
+
+if [ "$1" != "" ]; then
+    start_remote $1
+else
+    for f in *; do
+	start_remote $f
+    done
+fi
+
+
+kdialog --title "Remote chromium" --passivepopup "Done!" 2
