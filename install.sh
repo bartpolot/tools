1dba4822ef4100f6f9d1b567e67c7f28ac94a2db
Bart Polot <bart.polot+voyager@gmail.com>
2015-10-05 02:34:07 +0200

    Exculde scripts from install using a regexp

--- install.sh
+++ install.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+# where to install, if not provided as the first argument
 TARGET=${1:-$HOME/bin}
 
+# regexp for excludes
+EXCLUDE="^install.sh$ ^meta-update.sh$"
+
 for i in *.sh; do
-    if [ "$i" = "$0" ]; then
+    skip="";
+    for j in $EXCLUDE; do
+	if [[ $i =~ $j ]]; then
+	    skip="yes"
+	    break
+	fi
+    done
+    if [[ $skip ]]; then
 	continue
     fi
+
     cp "$i" "$TARGET/${i%%.sh}"
 done
