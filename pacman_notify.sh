2efcef3b2a24799c6fa498acac3b7d7709cced3c
Bart Polot <bart.polot+voyager@gmail.com>
2016-12-18 13:00:45 +0100

    set path to use other tools from same repo

--- pacman_notify.sh
+++ pacman_notify.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+PATH="`dirname $0`:$PATH"
+XDG_RUNTIME_DIR=/run/user/$UID
+DISPLAY=:0
+
 N=`pacman_count_updates`
 
 if [ $N -eq 0 ]; then
-    echo "Up to date!"
+    echo "`date` - Up to date!"
     exit 0
 fi
 
@@ -11,7 +15,4 @@ for p in `pacman -Que | grep -v '\[ignored\]' | cut -d' ' -f 1`; do
     LINE=$LINE${LINE+ / }$p
 done
 
-
-export XDG_RUNTIME_DIR=/run/user/$UID
-export DISPLAY=:0
 kdialog --title "Pacman: $N updates, most important:" --passivepopup "$LINE" 10
