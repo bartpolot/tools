922a45c7a0d31c3ffb49e2d68c712d9775eff44f
Bart Polot <bart.polot+voyager@gmail.com>
2016-12-21 23:54:34 +0100

    Fix pacman_sync to work from cron/timer
    * Explicitly read config data
    * Exit when network is accepted
    * Simplify control flow

--- pacman_sync.sh
+++ pacman_sync.sh
@@ -1,46 +1,62 @@
 #!/bin/sh
 
+do_sync ()
+{
+    echo Synching pacman at `date "+%H:%M"` on $network...
+    timeout 30 sudo pacman -Sy > /dev/null 2>&1;
+    exit 0
+}
+
+
+PATH="`dirname $0`:$PATH"
+
 # Set the following variables in your environment:
+# PACMAN_SYNC_ETH="eth0 en0"
 # PACMAN_SYNC_WIFI="wlan0 wlan1 ath0"
 # PACMAN_SYNC_SSID="linksys attwifi eduroam"
+config_file="$HOME/.config/pacman_sync.conf"
+if [ -f $config_file ]; then
+    source $config_file
+fi
 
-R="`which route` -n"
+r="`which route` -n"
 
 # Don't sync when offline
-if ! $R | cut -f 1 -d' ' | grep 0.0.0.0 > /dev/null; then
+if ! $r | cut -f 1 -d' ' | grep -q 0.0.0.0; then
     echo No gateway, not synching.
     exit 0
 fi
 
 # Don't sync on modem/3G connections
-if $R | grep ppp0 > /dev/null; then
+if $r | grep -q ppp0; then
     echo PPP detected, not synching.
     exit 0
 fi
 
-
-CHECK='yes'
-
-SSID="wired"
-GWIF=`gwif`
-for iface in $PACMAN_SYNC_WIFI; do
-    if [ "$GWIF" = "$iface" ]; then
-	SSID=`ssid $iface`
-	_SSID=`echo $SSID | tr ' ' _`
-	CHECK='no'
-	for name in $PACMAN_SYNC_SSID; do
-	    if [ "$name" = "$_SSID" ]; then
-		CHECK='yes'
-		break 2;
-	    fi
-	done
+gwif=`gwif`
+network='unknown'
+for iface in $PACMAN_SYNC_ETH; do
+    if [ "$gwif" = "$iface" ]; then
+	network="wired $iface"
+	do_sync
     fi
 done
 
-if [ "$CHECK" != 'yes' ]; then
-    echo Unknown wifi $SSID, not synching.
-    exit 0
+if [ "$check" != "yes" ]; then
+    for iface in $PACMAN_SYNC_WIFI; do
+	if [ "$gwif" = "$iface" ]; then
+	    network=`ssid $iface`
+	    _network=`echo $network | tr ' ' _`
+	    for name in $PACMAN_SYNC_SSID; do
+		if [ "$name" = "$_network" ]; then
+		    network="wifi $network"
+		    do_sync
+		fi
+	    done
+	    echo Unknown wifi $network, not synching.
+	    exit 0
+	fi
+    done
 fi
 
-echo Synching pacman at `date "+%H:%M"` on $SSID...
-timeout 30 sudo pacman -Sy > /dev/null 2>&1;
+echo Unknown default interface $gwif, not synching.
