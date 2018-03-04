52cc31de2b190dc1aca2e2556e3df3ad57d14e82
Bart Polot <bart.polot@gmail.com>
2018-03-04 17:51:46 +0100

    Mute the 0 sink (speakers) when resetting bt audio via 'off', to avoid a loud noise burst

--- a2dp_reset.sh
+++ a2dp_reset.sh
@@ -4,6 +4,9 @@ bluezcard=`echo "$info" | grep Name | tail -n 1 | egrep -o 'bluez.*'`
 if echo "$info" | grep -q "headset_head_unit.*available: yes"; then
     pactl set-card-profile $bluezcard headset_head_unit
 else
+    pactl set-sink-mute 0 1
+    sleep 0.1
     pactl set-card-profile $bluezcard off
+    pactl set-sink-mute 0 0
 fi
 pactl set-card-profile $bluezcard a2dp_sink
